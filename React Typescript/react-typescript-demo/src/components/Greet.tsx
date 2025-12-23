ccc001d44aee1397e8a4bb1e9c99b5350a09b1d5
rutujamokale <rutujamokale2003@gmail.com>
2025-12-23 17:13:58 +0530

    adding

--- React Typescript/react-typescript-demo/src/components/Greet.tsx
+++ React Typescript/react-typescript-demo/src/components/Greet.tsx
@@ -1,7 +1,10 @@
-export const Greet= () =>{
+type GreetProps={
+    name:string
+}
+export const Greet= (props:GreetProps) =>{
     return(
         <div>
-            <h2>Welcome Rutuja</h2>
+            <h2>Welcome {props.name}!</h2>
         </div>
     )
 }
