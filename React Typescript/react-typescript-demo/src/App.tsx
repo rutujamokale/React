4e101ca9aaaaa370962b88c10982a1384c518c7b
rutujamokale <rutujamokale2003@gmail.com>
2025-12-24 14:52:23 +0530

    changes

--- React Typescript/react-typescript-demo/src/App.tsx
+++ React Typescript/react-typescript-demo/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
   const personName = {
@@ -17,12 +18,17 @@ function App() {
       first: 'Rushi',
       last: 'Mokale'
     },
+    {
+      first: 'Rohan',
+      last: 'Mokale'
+    },
   ]
 
   return (
     <div className="App">
       <Greet name="Rutuja" age={23} isloggedin={true} />
       <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   )
 }
