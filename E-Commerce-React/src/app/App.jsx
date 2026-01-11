352692223ad0c80c0255d5e7637671933f1c5c9a
rutujamokale <rutujamokale2003@gmail.com>
2026-01-11 19:53:32 +0530

    e commerce using react

--- E-Commerce-React/src/app/App.jsx
+++ E-Commerce-React/src/app/App.jsx
@@ -1,17 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
-import "../styles/global.css";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main style={{ minHeight: "80vh", padding: "1rem" }}>
-        <AppRoutes />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <main style={{ minHeight: "80vh", padding: "1rem" }}>
+          <AppRoutes />
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
