99eb2d59fed003e8728d283178fc786bd8e0f5a6
rutujamokale <rutujamokale2003@gmail.com>
2026-01-16 15:33:20 +0530

    sidebar.jsx

---
+++ student-dashboard/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Sidebar from './components/layout/Sidebar'
+
+function App() {
+
+
+  return (
+    <>
+      <h2>sidebar</h2>
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
