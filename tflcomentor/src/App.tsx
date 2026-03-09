3b9ca3e8fad68f0ea419bffb7adeb820aabc5b0b
rutujamokale <rutujamokale2003@gmail.com>
2026-03-09 16:16:20 +0530

    add question type

--- tflcomentor/src/App.tsx
+++ tflcomentor/src/App.tsx
@@ -1,13 +1,18 @@
 import AddTechnology from './components/AddTechnology';
 import UpdateTechnology from './components/UpdateTechnology';
 import DeleteTechnology from './components/DeleteTechnology';
+import AddQuestionType from './components/AddQuestionType';
+import QuestionTypeManager from './components/UpdateQuestionType';
+import UpdateQuestionType from './components/UpdateQuestionType';
 
 function App() {
   return (
     <>
-      <AddTechnology />
+      {/* <AddTechnology />
       <UpdateTechnology />
-      <DeleteTechnology/>
+      <DeleteTechnology /> */}
+      {/* { <AddQuestionType/> } */}
+      <UpdateQuestionType/>
     </>
   );
 }
